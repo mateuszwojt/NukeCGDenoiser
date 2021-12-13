062acc9d2d09758f46d440a70c143e98f14739d6
Mateusz Wojt <mateusz.wojt@outlook.com>
2021-12-13 13:27:36 +0100

    fix oidn directory

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # download OIDN
 wget -q -P /tmp/ https://github.com/OpenImageDenoise/oidn/releases/download/v1.4.2/oidn-1.4.2.x86_64.linux.tar.gz
-mkdir -p /opt/oidn
-tar -C /opt/oidn -xvzf /tmp/oidn-1.4.2.x86_64.linux.tar.gz
+mkdir -p /opt
+tar -C /opt -xvzf /tmp/oidn-1.4.2.x86_64.linux.tar.gz
 
 # download and extract Nuke
 wget -q -P /tmp/ https://thefoundry.s3.amazonaws.com/products/nuke/releases/12.2v5/Nuke-12.2v5-linux-x86-64-installer.tgz
@@ -13,7 +13,7 @@ rm -vf /tmp/*
 # build plugin
 cd $CI_PROJECT_DIR
 mkdir build && cd build
-cmake -DOPENIMAGEDENOISE_ROOT_DIR=/opt/oidn ..
+cmake -DOPENIMAGEDENOISE_ROOT_DIR=/opt/oidn-1.4.2.x86_64.linux ..
 make
 make install
 cd ..
