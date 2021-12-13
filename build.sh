697bb0e88616f9c83950c5a35e7e5f14290ff7b1
Mateusz Wojt <mateusz.wojt@outlook.com>
2021-12-13 13:16:58 +0100

    download oidn binaries, update Nuke to 12.2

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
+# download OIDN
+wget -q -P /tmp/ https://github.com/OpenImageDenoise/oidn/releases/download/v1.4.2/oidn-1.4.2.x86_64.linux.tar.gz
+tar -C /opt/oidn -xvzf /tmp/oidn-1.4.2.x86_64.linux.tar.gz
+
 # download and extract Nuke
-wget -q -P /tmp/ https://thefoundry.s3.amazonaws.com/products/nuke/releases/12.1v2/Nuke-12.1v2-linux-x86-64-installer.tgz
-tar -C /tmp -xvzf /tmp/Nuke-12.1v2-linux-x86-64-installer.tgz
+wget -q -P /tmp/ https://thefoundry.s3.amazonaws.com/products/nuke/releases/12.2v5/Nuke-12.2v5-linux-x86-64-installer.tgz
+tar -C /tmp -xvzf /tmp/Nuke-12.2v5-linux-x86-64-installer.tgz
 cd /tmp
-./Nuke-12.1v2-linux-x86-64-installer.run --accept-foundry-eula --prefix=/usr/local
+./Nuke-12.2v5-linux-x86-64-installer.run --accept-foundry-eula --prefix=/usr/local
 rm -vf /tmp/*
 
 # build plugin
 cd $CI_PROJECT_DIR
 mkdir build && cd build
-cmake ..
+cmake -DOPENIMAGEDENOISE_ROOT_DIR=/opt/oidn ..
 make
 make install
 cd ..
