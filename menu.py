5168713b8c2369d01d72c97b75445c7250578554
Mateusz Wojt <mateusz.wojt@outlook.com>
2021-12-28 03:32:15 +0100

    cleanup import

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-plugin_path = os.path.dirname(os.path.realpath(__file__))
+import nuke
 
 nuke.menu('Nodes').addMenu('MW')
 nuke.menu('Nodes').addCommand('MW/Denoiser', lambda: nuke.createNode('Denoiser'))
