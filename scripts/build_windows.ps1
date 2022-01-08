9b08878c7081238983c828bed8ba10fa6dfcc3a1
Mateusz Wojt <mateusz.wojt@outlook.com>
2022-01-08 01:35:38 +0100

    split two commands

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -4,7 +4,8 @@ Expand-Archive "C:\oidn-1.4.2.x64.vc14.windows.zip" -DestinationPath "C:\oidn-1.
 
 # build plugin
 cd $CI_PROJECT_DIR
-mkdir build && cd build
+mkdir build
+cd build
 cmake -G "Visual Studio 14 2015 Win64" -DOPENIMAGEDENOISE_ROOT_DIR="C:\oidn-1.4.2.x64.vc14.windows" ..
 cmake --build . --config Release
 cd ..
