5c06c12c6f7abdde18df324ca8991ce040668964
Mateusz Wojt <mateusz.wojt@outlook.com>
2022-02-10 00:09:00 +0100

    add Nuke include dir

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -6,6 +6,6 @@ Expand-Archive "C:\oidn-1.4.2.x64.vc14.windows.zip" -DestinationPath "C:\oidn-1.
 cd $CI_PROJECT_DIR
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -DOPENIMAGEDENOISE_ROOT_DIR="C:\oidn-1.4.2.x64.vc14.windows" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 16 2019" -DNuke_INCLUDE_DIR="C:\Program Files\Nuke12.2v10\include" -DOPENIMAGEDENOISE_ROOT_DIR="C:\oidn-1.4.2.x64.vc14.windows" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 cd ..
