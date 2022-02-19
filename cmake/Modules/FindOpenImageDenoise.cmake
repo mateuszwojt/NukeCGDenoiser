5a64df958a571de4948bd5a4297b2117492af28f
Mateusz Wojt <mateusz.wojt@outlook.com>
2022-02-20 00:36:26 +0100

    write my own cmake module for finding OIDN

--- cmake/Modules/FindOpenImageDenoise.cmake
+++ cmake/Modules/FindOpenImageDenoise.cmake
@@ -1,122 +1,37 @@
-# - Find OpenImageDenoise library
-# Find the native OpenImageDenoise includes and library
-# This module defines
-#  OPENIMAGEDENOISE_INCLUDE_DIRS, where to find oidn.h, Set when
-#                    OPENIMAGEDENOISE is found.
-#  OPENIMAGEDENOISE_LIBRARIES, libraries to link against to use OpenImageDenoise.
-#  OPENIMAGEDENOISE_ROOT_DIR, The base directory to search for OpenImageDenoise.
-#                This can also be an environment variable.
-#  OPENIMAGEDENOISE_FOUND, If false, do not try to use OpenImageDenoise.
-#
-# also defined, but not for general use are
-#  OPENIMAGEDENOISE_LIBRARY, where to find the OpenImageDenoise library.
+if(NOT DEFINED OIDN_ROOT)
+  if(WIN32)
+    set(OIDN_ROOT "C:/oidn")
+  elseif(APPLE)
+    set(OIDN_ROOT "/opt/homebrew/Cellar/open-image-denoise")
+  else()
+    set(OIDN_ROOT "/usr/local/lib64")
+  endif()
+endif()
 
-#=============================================================================
-# Copyright 2019 Blender Foundation.
-#
-# Distributed under the OSI-approved BSD 3-Clause License,
-# see accompanying file BSD-3-Clause-license.txt for details.
-#=============================================================================
-
-# If OPENIMAGEDENOISE_ROOT_DIR was defined in the environment, use it.
-IF(NOT OPENIMAGEDENOISE_ROOT_DIR AND NOT $ENV{OPENIMAGEDENOISE_ROOT_DIR} STREQUAL "")
-  SET(OPENIMAGEDENOISE_ROOT_DIR $ENV{OPENIMAGEDENOISE_ROOT_DIR})
-ENDIF()
-
-SET(_openimagedenoise_SEARCH_DIRS
-  ${OPENIMAGEDENOISE_ROOT_DIR}
-  /opt/lib/openimagedenoise
+find_path(
+  OpenImageDenoise_INCLUDE_DIR
+  NAMES OpenImageDenoise/oidn.hpp
+  PATHS ${OIDN_ROOT}/include/
+  NO_DEFAULT_PATH
 )
 
-FIND_PATH(OPENIMAGEDENOISE_INCLUDE_DIR
-  NAMES
-    OpenImageDenoise/oidn.h
-  HINTS
-    ${_openimagedenoise_SEARCH_DIRS}
-  PATH_SUFFIXES
-    include
+if(OpenImageDenoise_INCLUDE_DIR)
+  include_directories(${OpenImageDenoise_INCLUDE_DIR})
+endif()
+
+find_library(
+  OpenImageDenoise_LIBRARIES
+  NAMES OpenImageDenoise
+  PATHS ${OIDN_ROOT}/lib
+  NO_DEFAULT_PATH
 )
 
-SET(_openimagedenoise_FIND_COMPONENTS
-  OpenImageDenoise
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OpenImageDenoise
+  FOUND_VAR
+    OpenImageDenoise_FOUND
+  REQUIRED_VARS
+    OIDN_ROOT
+    OpenImageDenoise_INCLUDE_DIR
+    OpenImageDenoise_LIBRARIES
 )
-
-# These are needed when building statically
-SET(_openimagedenoise_FIND_STATIC_COMPONENTS
-  common
-
-  # These additional library names change between versions, we list all of them
-  # so builds work with multiple versions. Missing libraries are skipped.
-  dnnl_cpu
-  dnnl_common
-  dnnl_cpu # Second time because of circular dependency
-  mkldnn
-  dnnl
-)
-
-SET(_openimagedenoise_LIBRARIES)
-FOREACH(COMPONENT ${_openimagedenoise_FIND_COMPONENTS})
-  STRING(TOUPPER ${COMPONENT} UPPERCOMPONENT)
-
-  FIND_LIBRARY(OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY
-    NAMES
-      ${COMPONENT}
-    HINTS
-      ${_openimagedenoise_SEARCH_DIRS}
-    PATH_SUFFIXES
-      lib64 lib
-    )
-  LIST(APPEND _openimagedenoise_LIBRARIES "${OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY}")
-ENDFOREACH()
-
-FOREACH(COMPONENT ${_openimagedenoise_FIND_STATIC_COMPONENTS})
-  STRING(TOUPPER ${COMPONENT} UPPERCOMPONENT)
-
-  FIND_LIBRARY(OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY
-    NAMES
-      ${COMPONENT}
-    HINTS
-      ${_openimagedenoise_SEARCH_DIRS}
-    PATH_SUFFIXES
-      lib64 lib
-    )
-  MARK_AS_ADVANCED(OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY)
-  IF(OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY)
-    LIST(APPEND _openimagedenoise_LIBRARIES "${OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY}")
-  ENDIF()
-ENDFOREACH()
-
-FIND_LIBRARY(OPENIMAGEDENOISE_LIBRARY
-  NAMES
-    OpenImageDenoise
-  HINTS
-    ${_openimagedenoise_SEARCH_DIRS}
-  PATH_SUFFIXES
-    lib64 lib
-  )
-
-# handle the QUIETLY and REQUIRED arguments and set OPENIMAGEDENOISE_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OpenImageDenoise DEFAULT_MSG
-    OPENIMAGEDENOISE_LIBRARY OPENIMAGEDENOISE_INCLUDE_DIR)
-
-IF(OPENIMAGEDENOISE_FOUND)
-  SET(OPENIMAGEDENOISE_LIBRARIES ${_openimagedenoise_LIBRARIES})
-  SET(OPENIMAGEDENOISE_INCLUDE_DIRS ${OPENIMAGEDENOISE_INCLUDE_DIR})
-ELSE()
-  SET(OPENIMAGEDENOISE_FOUND FALSE)
-ENDIF()
-
-MARK_AS_ADVANCED(
-  OPENIMAGEDENOISE_INCLUDE_DIR
-)
-
-FOREACH(COMPONENT ${_openimagedenoise_FIND_COMPONENTS})
-  STRING(TOUPPER ${COMPONENT} UPPERCOMPONENT)
-  MARK_AS_ADVANCED(OPENIMAGEDENOISE_${UPPERCOMPONENT}_LIBRARY)
-ENDFOREACH()
-
-UNSET(_openimagedenoise_SEARCH_DIRS)
-UNSET(_openimagedenoise_FIND_COMPONENTS)
-UNSET(_openimagedenoise_LIBRARIES)
--- scripts/linux_build_plugin.sh
+++ scripts/linux_build_plugin.sh
@@ -10,7 +10,7 @@ rm -vf /tmp/*
 # build plugin
 cd $CI_PROJECT_DIR
 mkdir build && cd build
-cmake -DOpenImageDenoise_ROOT_DIR=/opt/oidn-1.4.2.x86_64.linux ..
+cmake -DNUKE_ROOT=/usr/local/Nuke12.2v10 -DOIDN_ROOT=/opt/oidn-1.4.2.x86_64.linux -DCMAKE_VERBOSE_MAKEFILE=ON ..
 make
 make install
 cd ..
