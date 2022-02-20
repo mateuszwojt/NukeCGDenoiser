fb600d9d0ad4e5f7572d1a53459e1e7ffd7174c0
Mateusz Wojt <mateusz.wojt@outlook.com>
2022-02-20 10:57:53 +0100

    try extracting oidn into project dir

--- scripts/windows_build_plugin.ps1
+++ scripts/windows_build_plugin.ps1
@@ -1,11 +1,11 @@
 # download OIDN
-Invoke-WebRequest -Uri "https://github.com/OpenImageDenoise/oidn/releases/download/v1.4.2/oidn-1.4.2.x64.vc14.windows.zip" -OutFile "C:\oidn-1.4.2.x64.vc14.windows.zip"
-Expand-Archive "C:\oidn-1.4.2.x64.vc14.windows.zip" -DestinationPath "C:\oidn-1.4.2.x64.vc14.windows"
+cd $CI_PROJECT_DIR
+Invoke-WebRequest -Uri "https://github.com/OpenImageDenoise/oidn/releases/download/v1.4.2/oidn-1.4.2.x64.vc14.windows.zip" -OutFile $CI_PROJECT_DIR\oidn-1.4.2.x64.vc14.windows.zip
+Expand-Archive $CI_PROJECT_DIR\oidn-1.4.2.x64.vc14.windows.zip -DestinationPath $CI_PROJECT_DIR\oidn-1.4.2.x64.vc14.windows -Verbose
 
 # build plugin
-cd $CI_PROJECT_DIR
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:/Program Files/Nuke12.2v10" -DOIDN_ROOT="C:/oidn-1.4.2.x64.vc14.windows" -DOpenImageDenoise_INCLUDE_DIR="C:/oidn-1.4.2.x64.vc14.windows/include" -DOpenImageDenoise_LIBRARIES="C:/oidn-1.4.2.x64.vc14.windows/lib/OpenImageDenoise.lib" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:/Program Files/Nuke12.2v10" -DOIDN_ROOT=$CI_PROJECT_DIR/oidn-1.4.2.x64.vc14.windows -DOpenImageDenoise_INCLUDE_DIR=$CI_PROJECT_DIR/oidn-1.4.2.x64.vc14.windows/include -DOpenImageDenoise_LIBRARIES=$CI_PROJECT_DIR/oidn-1.4.2.x64.vc14.windows/lib/OpenImageDenoise.lib -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 cd ..
