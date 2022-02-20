bec73ae7ba523c8f5ef5cc977faddead1c633528
Mateusz Wojt <mateusz.wojt@outlook.com>
2022-02-20 09:34:02 +0100

    add include and library paths

--- scripts/windows_build_plugin.ps1
+++ scripts/windows_build_plugin.ps1
@@ -6,6 +6,6 @@ Expand-Archive "C:\oidn-1.4.2.x64.vc14.windows.zip" -DestinationPath "C:\oidn-1.
 cd $CI_PROJECT_DIR
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:/Program Files/Nuke12.2v10" -DOIDN_ROOT="C:/oidn-1.4.2.x64.vc14.windows" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:/Program Files/Nuke12.2v10" -DOIDN_ROOT="C:/oidn-1.4.2.x64.vc14.windows" -DOpenImageDenoise_INCLUDE_DIR="C:/oidn-1.4.2.x64.vc14.windows/include" -DOpenImageDenoise_LIBRARIES="C:/oidn-1.4.2.x64.vc14.windows/lib/OpenImageDenoise.lib" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 cd ..
