9cc06b96acd7050a4fd935cefd9c9e76c940ef49
Mateusz Wojt <mateusz.wojt@outlook.com>
2022-02-20 12:52:57 +0100

    use backslashes

--- scripts/windows_build_plugin.ps1
+++ scripts/windows_build_plugin.ps1
@@ -6,6 +6,6 @@ Expand-Archive "C:\oidn-1.4.2.x64.vc14.windows.zip" -DestinationPath "C:\" -Verb
 cd $CI_PROJECT_DIR
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:/Program Files/Nuke12.2v10" -DOIDN_ROOT="C:/oidn-1.4.2.x64.vc14.windows" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:\Program Files\Nuke12.2v10" -DOIDN_ROOT="C:\oidn-1.4.2.x64.vc14.windows" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 cd ..
