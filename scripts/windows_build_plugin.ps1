455396f4a7320d0d492e0bb47755bdcbb235cf3e
Mateusz Wojt <mateusz.wojt@outlook.com>
2024-01-02 20:45:55 +0100

    update OIDN version to 2.1.0

--- scripts/windows_build_plugin.ps1
+++ scripts/windows_build_plugin.ps1
@@ -1,11 +1,11 @@
 # download OIDN
-Invoke-WebRequest -Uri "https://github.com/OpenImageDenoise/oidn/releases/download/v1.4.3/oidn-1.4.3.x64.vc14.windows.zip" -OutFile "C:\oidn-1.4.3.x64.vc14.windows.zip"
-Expand-Archive "C:\oidn-1.4.3.x64.vc14.windows.zip" -DestinationPath "C:\" -Verbose
+Invoke-WebRequest -Uri "https://github.com/OpenImageDenoise/oidn/releases/download/v2.1.0/oidn-2.1.0.x64.windows.zip" -OutFile "C:\oidn-2.1.0.x64.windows.zip"
+Expand-Archive "C:\oidn-2.1.0.x64.windows.zip" -DestinationPath "C:\" -Verbose
 
 # build plugin
 cd $CI_PROJECT_DIR
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:\Program Files\Nuke12.2v10" -DNuke_INCLUDE_DIR="C:\Program Files\Nuke12.2v10\include" -DOIDN_ROOT="C:\oidn-1.4.3.x64.vc14.windows" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Visual Studio 16 2019" -DNUKE_ROOT="C:\Program Files\Nuke12.2v10" -DNuke_INCLUDE_DIR="C:\Program Files\Nuke12.2v10\include" -DOIDN_ROOT="C:\oidn-2.1.0.x64.windows" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 cd ..
